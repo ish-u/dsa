9d9a57e0e784f18900b2fb149a47d367ee2b767b
ish-u <anmolgupta520@gmail.com>
2022-07-29 23:12:44 +0530

    refactor : added height, min, max, deleteNode  methods

--- tree.h
+++ tree.h
@@ -17,7 +17,7 @@ public:
     // destructor
     ~TreeNode()
     {
-        std ::cout << "DELETEING " << this << "\n";
+        // std ::cout << "DELETEING " << this << "\n";
     }
 };
 
@@ -33,6 +33,10 @@ private:
     void inorder(TreeNode<T> *root);
     void preorder(TreeNode<T> *root);
     void postorder(TreeNode<T> *root);
+    int findHeight(TreeNode<T> *node);
+    T max(TreeNode<T> *root);
+    T min(TreeNode<T> *root);
+    void deleteNodeHelper(TreeNode<T> *root, T ele);
 
 public:
     Tree();
@@ -40,7 +44,7 @@ public:
     // member functions
     void insert(T data);
     void insertRec(T data);
-    int search(T data);
+    void search(T data);
     void print();
     void inorderRecursive();
     void preorderRecursive();
@@ -52,8 +56,7 @@ public:
     int leafNodes();
     int internalNodes();
     int height();
-    void deleteNodeMerging();
-    void deleteNodeCopying();
+    void deleteNode(T ele);
     void findAndReplace();
     void clear();
 };
@@ -70,6 +73,29 @@ template <typename T>
 Tree<T>::~Tree()
 {
     std ::cout << "DELETING " << this << "\n";
+}
+
+template <typename T>
+void Tree<T>::search(T ele)
+{
+    TreeNode<T> *temp = root;
+    while (temp != nullptr)
+    {
+        if (temp->data == ele)
+        {
+            std ::cout << "ELEMENT FOUND\n";
+            return;
+        }
+        if (ele > temp->data)
+        {
+            temp = temp->right;
+        }
+        else if (ele < temp->data)
+        {
+            temp = temp->left;
+        }
+    }
+    std ::cout << "ELEMENT NOT FOUND\n";
 }
 
 // insert
@@ -301,47 +327,25 @@ void Tree<T>::inorderIterative()
     if (this->root != nullptr)
     {
         StackDynamic<TreeNode<T> *> S1;
-        StackDynamic<TreeNode<T> *> S2;
-        StackDynamic<TreeNode<T> *> S3;
-        S1.push(root);
-        while (S1.isEmpty() == 0)
+        // StackDynamic<TreeNode<T> *> S2;
+        // StackDynamic<TreeNode<T> *> S3;
+        // S1.push(root);
+        TreeNode<T> *tos = root;
+        while (S1.isEmpty() == 0 || tos != nullptr)
         {
-            TreeNode<T> *tos = S1.top();
-            while (tos->left != nullptr)
+            while (tos != nullptr)
             {
-                S1.push(tos->left);
+                S1.push(tos);
                 tos = tos->left;
             }
-            while (S1.isEmpty() == 0 && S1.top()->right == nullptr)
-            {
-                TreeNode<T> *tos = S1.top();
-                S2.push(tos);
-                S1.pop();
-                if (S1.isEmpty() == 0 && S1.top()->right != nullptr)
-                {
-                    tos = S1.top();
-                    S2.push(tos);
-                    S1.pop();
-                    S1.push(S2.top()->right);
-                }
-            }
-        }
-        while (S2.isEmpty() == 0)
-        {
-            S3.push(S2.top());
-            S2.pop();
-        }
-        while (S3.isEmpty() == 0)
-        {
-
-            TreeNode<T> *tos = S3.top();
+            tos = S1.top();
+            S1.pop();
             std ::cout << tos->data << "  ";
-            S3.pop();
+            tos = tos->right;
         }
+        std ::cout << "\n";
     }
-    std ::cout << "\n";
 }
-
 // level order traversal
 template <typename T>
 void Tree<T>::levelOrder()
@@ -372,5 +376,157 @@ void Tree<T>::levelOrder()
             }
         }
         std ::cout << "\n";
+    }
+}
+
+// min
+template <typename T>
+T Tree<T>::min(TreeNode<T> *root)
+{
+    TreeNode<T> *node = root;
+    while (node->left != nullptr)
+    {
+        node = node->left;
+    }
+    // std ::cout << "MIN : " << node->data << "\n";
+    return node->data;
+}
+
+// max
+template <typename T>
+T Tree<T>::max(TreeNode<T> *root)
+{
+    TreeNode<T> *node = root;
+    while (node->right != nullptr)
+    {
+        node = node->right;
+    }
+    // std ::cout << "MAX : " << node->data << "\n";
+    return node->data;
+}
+
+// height
+template <typename T>
+int Tree<T>::findHeight(TreeNode<T> *root)
+{
+    if (root == nullptr)
+    {
+        return -1;
+    }
+    int heightL = findHeight(root->left);
+    int heightR = findHeight(root->right);
+    int height = heightL > heightR ? heightL : heightR;
+    return 1 + height;
+}
+
+template <typename T>
+int Tree<T>::height()
+{
+    std ::cout << findHeight(root) << "\n";
+    return findHeight(root);
+}
+
+// delete and merge
+template <typename T>
+void Tree<T>::deleteNode(T ele)
+{
+    /*
+        1. Search the Node to be deleted
+        2.
+            2.0 CASE 0 : If Node == 'root' jump to CASE 3
+            2.1 CASE 1 : No Child
+                Delete the Node as it's a Leaf Node
+            2.2 CASE 2 : ONE CHILD
+                Set the Child of the Node's Parent as Node's Child
+                and delete the Node
+            2.3 CASE 3 : 2 CHILD
+                     3.1 -> Get the Max Value form LEFT SubTree
+                  OR 3.1 -> Get the Min Value from the RIGHT Subtree
+                     3.2 -> Replace the Value to Node to be deleted by this Value
+                     3.3 -> Recursively Delete the Node with the Value that was used to
+                            replace the Node to be deleted
+    */
+    deleteNodeHelper(this->root, ele);
+}
+
+template <typename T>
+void Tree<T>::deleteNodeHelper(TreeNode<T> *root, T ele)
+{
+    TreeNode<T> *parent = nullptr;
+    TreeNode<T> *node = root;
+    int flag = 0;
+    while (node != nullptr)
+    {
+        if (node->data == ele)
+        {
+            flag = 1;
+            break;
+        }
+        else if (ele > node->data)
+        {
+            parent = node;
+            node = node->right;
+        }
+        else if (ele < node->data)
+        {
+            parent = node;
+            node = node->left;
+        }
+    }
+
+    if (flag)
+    {
+        // CASE 0
+        if (root == node)
+        {
+            T minNodeOnRightSubtree = min(node->right);
+            node->data = minNodeOnRightSubtree;
+            deleteNodeHelper(node->right, node->data);
+        }
+        // CASE 1
+        else if (node->left == nullptr && node->right == nullptr)
+        {
+            if (parent->left == node)
+            {
+                parent->left = nullptr;
+            }
+            else if (parent->right == node)
+            {
+                parent->right = nullptr;
+            }
+            delete node;
+        }
+        // CASE 2
+        else if (node->left == nullptr && node->right != nullptr)
+        {
+            if (parent->left == node)
+            {
+                parent->left = node->right;
+            }
+            else if (parent->right == node)
+            {
+                parent->right = node->right;
+            }
+            delete node;
+        }
+        else if (node->left != nullptr && node->right == nullptr)
+        {
+            if (parent->left == node)
+            {
+                parent->left = node->left;
+            }
+            else if (parent->right == node)
+            {
+                parent->right = node->left;
+            }
+            delete node;
+        }
+        // CASE 3
+        else if (node->left != nullptr && node->right != nullptr)
+        {
+            T minNodeOnRightSubtree = min(node->right);
+            node->data = minNodeOnRightSubtree;
+            deleteNodeHelper(node->right, node->data);
+        }
     }
 }
